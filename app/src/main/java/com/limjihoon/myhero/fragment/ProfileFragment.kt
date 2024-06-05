4aa1c2c816e404d0c1ec0da4ce8481d197bcdfe4
dlawlgns1994 <dlawlgns1994@gmail.com>
2024-06-05 17:57:57 +0900

    다이얼로그 완

--- app/src/main/java/com/limjihoon/myhero/fragment/ProfileFragment.kt
+++ app/src/main/java/com/limjihoon/myhero/fragment/ProfileFragment.kt
@@ -1,27 +1,76 @@
 package com.limjihoon.myhero.fragment
 
+
+import android.app.AlertDialog
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.Button
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.limjihoon.myhero.R
 import com.limjihoon.myhero.databinding.FragmentProfileBinding
 
+
 class ProfileFragment : Fragment(){
     lateinit var binding: FragmentProfileBinding
+    private var loadingDialog: AlertDialog? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding= FragmentProfileBinding.inflate(layoutInflater,container,false)
-        binding.rendumBtn.setOnClickListener { rendum() }
+        binding.rendumBtn.setOnClickListener { dilrendum() }
         return binding.root
     }
+    private fun dilrendum() {
+        Handler(Looper.getMainLooper()).postDelayed({
+//            showLoadingDialog()
+            rendum()
+        }, 3000) // 3000 milliseconds delay
+    }
+
+
+//    private fun showLoadingDialog() {
+//        val builder = AlertDialog.Builder(requireContext())
+//        val inflater = layoutInflater
+//        val dialogView = inflater.inflate(R.layout.loding_layout, null)
+//        builder.setView(dialogView)
+//        builder.setCancelable(false)
+//
+//        val loadingGif: ImageView = dialogView.findViewById(R.id.loadingGif)
+//        Glide.with(this).asGif().load(R.drawable.loding).into(loadingGif)
+//
+//        loadingDialog = builder.create()
+//        loadingDialog?.show()
+//    }
+//
+//    private fun hideLoadingDialog() {
+//        loadingDialog?.dismiss()
+//    }
+//
+
+
     private fun rendum(){
-        Toast.makeText(requireContext(), "렌덤 돌린다냥~", Toast.LENGTH_SHORT).show()
+        val builder = AlertDialog.Builder(requireContext())
+        val inflater = layoutInflater
+        val dialogView = inflater.inflate(R.layout.custum_dialog_rendum, null)
+        builder.setView(dialogView)
+
+        val dialog = builder.create()
+        dialog.show()
+
+        val dialogButton: Button = dialogView.findViewById(R.id.dialogButton)
+        dialogButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
     }
 
 }
