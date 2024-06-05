7c85d849056b56cd981397946aba5ee214111bb3
dlawlgns1994 <dlawlgns1994@gmail.com>
2024-06-05 16:37:07 +0900

    렌덤과 쳇봇 엑티비티

--- app/src/main/java/com/limjihoon/myhero/fragment/HomeFragment.kt
+++ app/src/main/java/com/limjihoon/myhero/fragment/HomeFragment.kt
@@ -1,11 +1,15 @@
 package com.limjihoon.myhero.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.limjihoon.myhero.R
+import com.limjihoon.myhero.activitis.ChatBotActivity
+import com.limjihoon.myhero.activitis.IntroActivity
+import com.limjihoon.myhero.databinding.ActivityIntroBinding
 import com.limjihoon.myhero.databinding.FragmentHomeBinding
 import com.limjihoon.myhero.databinding.FragmentSearchBinding
 
@@ -17,6 +21,7 @@ class HomeFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(layoutInflater,container,false)
+        binding.fabtn.setOnClickListener { startActivity(Intent(requireContext(),ChatBotActivity::class.java)) }
         return binding.root
 
     }
