f222d86fd4329ed8cd71b1819584fb1a285b4d52
im62 <yeongseunggim62@gmail.com>
2024-06-18 10:51:23 +0900

    영짱 지훈짱 퓨 전~

--- app/src/main/java/com/limjihoon/myhero/fragment/HomeFragment.kt
+++ app/src/main/java/com/limjihoon/myhero/fragment/HomeFragment.kt
@@ -14,6 +14,7 @@ import androidx.fragment.app.Fragment
 import com.limjihoon.myhero.G
 import com.limjihoon.myhero.R
 import com.limjihoon.myhero.activitis.ChatBotActivity
+import com.limjihoon.myhero.activitis.MainActivity
 import com.limjihoon.myhero.activitis.MapActivity
 import com.limjihoon.myhero.adapter.TodoRecyclerAdapter
 import com.limjihoon.myhero.data.Member2
