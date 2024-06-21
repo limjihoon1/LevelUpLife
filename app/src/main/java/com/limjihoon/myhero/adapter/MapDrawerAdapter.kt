4df1c0589bd4076a506354ef1c40424b7271ee02
dlawlgns1994 <dlawlgns1994@gmail.com>
2024-06-21 19:32:57 +0900

    맵 끝

--- app/src/main/java/com/limjihoon/myhero/adapter/MapDrawerAdapter.kt
+++ app/src/main/java/com/limjihoon/myhero/adapter/MapDrawerAdapter.kt
@@ -4,13 +4,25 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.recyclerview.widget.RecyclerView.Adapter
 import com.limjihoon.myhero.data.Markers
-
 import com.limjihoon.myhero.databinding.RecyclMapBinding
 
-class MapDrawerAdapter(val context: Context, val items: MutableList<Markers>) :Adapter<MapDrawerAdapter.VH>(){
-    inner class VH(val binding: RecyclMapBinding) : RecyclerView.ViewHolder(binding.root)
+class MapDrawerAdapter(
+    val context: Context,
+    val items: MutableList<Markers>,
+    private val itemClickListener: OnItemClickListener
+) : RecyclerView.Adapter<MapDrawerAdapter.VH>() {
+
+    inner class VH(val binding: RecyclMapBinding) : RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    itemClickListener.onItemClick(items[position])
+                }
+            }
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
         return VH(RecyclMapBinding.inflate(LayoutInflater.from(context), parent, false))
@@ -21,8 +33,11 @@ class MapDrawerAdapter(val context: Context, val items: MutableList<Markers>) :A
     }
 
     override fun onBindViewHolder(holder: VH, position: Int) {
-        var item = items[position]
+        val item = items[position]
         holder.binding.tvMapWorkTodo.text = item.workTodo
+    }
 
+    interface OnItemClickListener {
+        fun onItemClick(marker: Markers)
     }
 }
