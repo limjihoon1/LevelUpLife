937e57d66d9b13735f0df2a751e406f97ecf89b7
ChoKwangEun1021 <rhkddms1021@gmail.com>
2024-06-26 12:12:11 +0900

    todo AI 분석 완료 UI 진짜 진짜 완료

--- app/src/main/java/com/limjihoon/myhero/GlobalApplication.kt
+++ app/src/main/java/com/limjihoon/myhero/GlobalApplication.kt
@@ -10,6 +10,6 @@ class GlobalApplication: Application() {
         //카카오 SDK 초기화 작업
 //        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_APP_KEY)
         //카카오맵 SDK 초기화 작업
-        KakaoMapSdk.init(this,"98be4a17c4968bce31a14570baa403a2" )
+        KakaoMapSdk.init(this, getString(R.string.kakao_map))
     }
 }
